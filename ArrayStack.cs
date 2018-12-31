4add0aed6633230216c5f3eec55b9526392e0af3
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-12-31 16:08:32 +0200

    Add fields, constructors, and method stubs to ArrayStack.

--- ArrayStack.cs
+++ ArrayStack.cs
@@ -2,7 +2,69 @@
 
 namespace DataStructures
 {
-    public class ArrayStack<T>
-    {
-    }
+	public class ArrayStack<T> : IStack<T>
+	{
+		private const int DEFAULT_CHUNK_SIZE = 10;
+
+		private T[] internalArray;
+		private bool chunkAllocation;
+		private int chunkSize;
+        private int lastElementIndex;
+
+		public ArrayStack () : this(false) { }
+
+		public ArrayStack (bool chunkAllocation) : this(chunkAllocation, DEFAULT_CHUNK_SIZE) { }
+
+		public ArrayStack (bool chunkAllocation, int chunkSize)
+		{
+            this.chunkAllocation = chunkAllocation;
+            this.chunkSize = chunkAllocation ? chunkSize : 1;
+            lastElementIndex = 0;
+
+            internalArray = new T[chunkSize];
+		}
+
+		public int Count
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public void Push (T element)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Pop ()
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Peek ()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Contains (T element)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T[] ToArray ()
+		{
+			throw new NotImplementedException();
+		}
+
+        private void enlargeInternalArray()
+        {
+			throw new NotImplementedException();
+        }
+
+        private void shrinkInternalArray()
+        {
+			throw new NotImplementedException();
+        }
+	}
 }

830568acfd5257ef53c6df460196f637e8cc5cc3
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-12-31 13:28:37 +0200

    Add IQueue interface.

---
+++ IQueue.cs
@@ -0,0 +1,22 @@
+namespace DataStructures
+{
+	public interface IQueue<T>
+	{
+		int Count
+		{
+			get;
+		}
+
+		void Enqueue (T element);
+
+		/// <exception cref="System.InvalidOperationException"> Thrown if queue is empty. </exception>
+		T Dequeue ();
+
+		/// <exception cref="System.InvalidOperationException"> Thrown if queue is empty. </exception>
+		T Peek ();
+
+		bool Contains (T element);
+
+		T[] ToArray ();
+	}
+}

a47682a81c69dd18eec093fc33e2a13536f10bf4
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-12-31 17:49:35 +0200

    Add ArrayStack test stubs.

---
+++ Tests/ArrayStackTests.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using DataStructures;
+
+namespace DataStructures.Tests
+{
+    [TestFixture]
+    public class ArrayStackTests
+    {
+        [Test]
+        public void CountTest()
+        {
+            
+        }
+
+        [Test]
+        public void PushTest()
+        {
+
+        }
+
+        [Test]
+        public void PopTest()
+        {
+
+        }
+
+        [Test]
+        public void PeekTest()
+        {
+
+        }
+
+        [Test]
+        public void ContainsTest()
+        {
+
+        }
+
+        [Test]
+        public void ToArrayTest()
+        {
+
+        }
+    }
+}

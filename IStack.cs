5417b3ab4735b18f3dd8d9370616248be4352515
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-12-31 13:28:30 +0200

    Add IStack interface.

---
+++ IStack.cs
@@ -0,0 +1,23 @@
+namespace DataStructures
+{
+	/// <summary> Generic interface for stack implementations. </summary>
+	public interface IStack<T>
+	{
+		int Count
+		{
+			get;
+		}
+
+		void Push (T element);
+
+		/// <exception cref="System.InvalidOperationException"> Thrown if stack is empty. </exception>
+		T Pop ();
+
+		/// <exception cref="System.InvalidOperationException"> Thrown if stack is empty. </exception>
+		T Peek ();
+
+		bool Contains (T element);
+
+		T[] ToArray ();
+	}
+}

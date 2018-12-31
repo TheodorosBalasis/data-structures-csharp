b039dd94d22173329a7a008f423f759a4c0f83fc
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-12-31 18:04:05 +0200

    Implement ArrayStack Count tests.

--- Tests/ArrayStackTests.cs
+++ Tests/ArrayStackTests.cs
@@ -7,10 +7,23 @@ namespace DataStructures.Tests
     public class ArrayStackTests
     {
         [Test]
-        public void CountTest()
+        public void CountTest1()
         {
+            ArrayStack<int> stack = new ArrayStack<int>();
             
+            Assert.That(stack.Count, Is.EqualTo(0));
         }
+
+		[Test]
+		public void CountTest2 ()
+		{
+			ArrayStack<int> stack = new ArrayStack<int>();
+
+			stack.Push(0);
+			stack.Push(1);
+
+			Assert.That(stack.Count, Is.EqualTo(2));
+		}
 
         [Test]
         public void PushTest()

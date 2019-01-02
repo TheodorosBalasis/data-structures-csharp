4dd5c0485bef75fae92d5cf6758a93f72280937d
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-03 00:58:55 +0200

    Create Linked List test stubs.

---
+++ Tests/LinkedListTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace DataStructures.Tests
+{
+    [TestFixture, TestOf(typeof(LinkedList<int>))]
+    public class LinkedListTests
+    {
+        [Test, Parallelizable]
+        public void AddTest()
+        {
+            
+        }
+
+        [Test, Parallelizable]
+        public void RemoveTest()
+        {
+
+        }
+
+        [Test, Parallelizable]
+        public void IndexerTest()
+        {
+            
+        }
+
+        [Test, Parallelizable]
+        public void ToArrayTest()
+        {
+
+        }
+    }
+}

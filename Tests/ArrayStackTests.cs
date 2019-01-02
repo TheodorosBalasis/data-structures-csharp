8a3c52992dd9cc45df5c19ba75430b0b24539f5e
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-03 00:49:28 +0200

    Implement ArrayStack tests.

--- Tests/ArrayStackTests.cs
+++ Tests/ArrayStackTests.cs
@@ -1,58 +1,63 @@
 using NUnit.Framework;
-using DataStructures;
 
 namespace DataStructures.Tests
 {
-    [TestFixture]
-    public class ArrayStackTests
-    {
-        [Test]
-        public void CountTest1()
-        {
-            ArrayStack<int> stack = new ArrayStack<int>();
-            
-            Assert.That(stack.Count, Is.EqualTo(0));
-        }
+	[TestFixture, TestOf(typeof(ArrayStack<int>))]
+	public class ArrayStackTests
+	{
+		[Test, Parallelizable]
+		public void ConstructorTest () => Assert.That(() => new ArrayStack<int>(null), Throws.ArgumentNullException);
 
-		[Test]
-		public void CountTest2 ()
+		[Test, Parallelizable]
+		public void CountTest1 ()
 		{
-			ArrayStack<int> stack = new ArrayStack<int>();
+			var stack = new ArrayStack<int>();
 
-			stack.Push(0);
-			stack.Push(1);
-
-			Assert.That(stack.Count, Is.EqualTo(2));
+			Assert.That(stack.Count, Is.EqualTo(0));
 		}
 
-        [Test]
-        public void PushTest()
-        {
+		[Test, Parallelizable]
+		public void CountTest2 ()
+		{
+			var stack = new ArrayStack<int>(new int[] { 0, 1, 2 });
 
-        }
+			Assert.That(stack.Count, Is.EqualTo(3));
+		}
 
-        [Test]
-        public void PopTest()
-        {
+		[Test, Parallelizable]
+		public void BasicFunctionsTest ()
+		{
+			var stack = new ArrayStack<int>();
 
-        }
+			stack.Push(1);
 
-        [Test]
-        public void PeekTest()
-        {
+			Assert.That(stack.Peek, Is.EqualTo(1));
+			Assert.That(stack.Pop, Is.EqualTo(1));
+		}
 
-        }
+		[Test, Parallelizable]
+		public void BasicFunctionsExceptionsTest ()
+		{
+			var stack = new ArrayStack<int>();
 
-        [Test]
-        public void ContainsTest()
-        {
+			Assert.That(stack.Peek, Throws.InvalidOperationException);
+			Assert.That(stack.Pop, Throws.InvalidOperationException);
+		}
 
-        }
+		[Test, Parallelizable]
+		public void ContainsTest ()
+		{
+			var stack = new ArrayStack<int>(new int[] { 0, 1, 2, 3 });
 
-        [Test]
-        public void ToArrayTest()
-        {
+			Assert.That(stack.Contains(1), Is.True);
+		}
 
-        }
-    }
+		[Test, Parallelizable]
+		public void ToArrayTest ()
+		{
+			var stack = new ArrayStack<int>(new int[] { 0, 1, 2, 3 });
+
+			Assert.That(stack.ToArray, Is.EqualTo(new int[] { 0, 1, 2, 3 }));
+		}
+	}
 }

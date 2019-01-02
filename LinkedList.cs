0f66e723218790a881ac7408cf2bde52642b2d26
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-03 01:02:26 +0200

    Add Linked List method and property stubs.

--- LinkedList.cs
+++ LinkedList.cs
@@ -2,6 +2,31 @@ namespace DataStructures
 {
     public class LinkedList<T>
     {
-        
+        public T this[int i]
+        {
+            get
+            {
+                return default(T);
+            }
+            set
+            {
+
+            }
+        }
+
+        public void Add()
+        {
+
+        }
+
+        public void Remove()
+        {
+
+        }
+
+        public void ToArray()
+        {
+
+        }
     }
 }

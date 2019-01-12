2d2f8f537a6f79993aa2eb1e7ddb2f031133e9b8
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-12 23:49:08 +0200

    Implement linked list node.

--- LinkedListNode.cs
+++ LinkedListNode.cs
@@ -1,7 +1,21 @@
 namespace DataStructures
 {
-    public class LinkedListNode<T>
-    {
-        
-    }
+	public class LinkedListNode<T>
+	{
+		public T Data { get; set; } = default(T);
+		public LinkedListNode<T> PreviousNode { get; set; } = null;
+		public LinkedListNode<T> NextNode { get; set; } = null;
+
+		public LinkedListNode (T data)
+		{
+			Data = data;
+		}
+
+		public LinkedListNode (T data, LinkedListNode<T> previousNode, LinkedListNode<T> nextNode)
+		{
+			Data = data;
+			PreviousNode = previousNode;
+			NextNode = nextNode;
+		}
+	}
 }

d1219b2ca46ced95eed334d937465fe05113f871
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-13 13:38:11 +0200

    Implement Count property and initial values constructor.

--- LinkedList.cs
+++ LinkedList.cs
@@ -1,32 +1,72 @@
+using System.Linq;
+
 namespace DataStructures
 {
-    public class LinkedList<T>
-    {
-        public T this[int i]
-        {
-            get
-            {
-                return default(T);
-            }
-            set
-            {
+	public class LinkedList<T>
+	{
+		private LinkedListNode<T> firstNode;
+		private LinkedListNode<T> lastNode;
 
-            }
-        }
+		public T this[int i]
+		{
+			get
+			{
+				return default(T);
+			}
+			set
+			{
 
-        public void Add()
-        {
+			}
+		}
 
-        }
+		public int Count
+		{
+			get
+			{
+				var count = 0;
 
-        public void Remove()
-        {
+				for (var node = firstNode; node != null; node = node.NextNode)
+					count++;
 
-        }
+				return count;
+			}
+		}
 
-        public void ToArray()
-        {
+		public LinkedList ()
+		{
 
-        }
-    }
+		}
+
+		public LinkedList (T[] initialElements)
+		{
+			if (initialElements.Length == 0)
+				return;
+
+			firstNode = new LinkedListNode<T>(initialElements[0]);
+            lastNode = firstNode;
+
+			foreach (var element in initialElements.TakeLast(initialElements.Length - 1))
+			{
+                var newNode = new LinkedListNode<T>(element);
+                newNode.PreviousNode = lastNode;
+                lastNode.NextNode = newNode;
+                lastNode = newNode;
+			}
+		}
+
+		public void Add ()
+		{
+
+		}
+
+		public void Remove ()
+		{
+
+		}
+
+		public void ToArray ()
+		{
+
+		}
+	}
 }

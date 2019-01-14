e221c12cb141388b6ed2211b861debe41f9046bd
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-14 18:01:17 +0200

    Add method stubs.

--- Source/ArrayQueue.cs
+++ Source/ArrayQueue.cs
@@ -1,7 +1,34 @@
+
+
 namespace DataStructures.Source
 {
-    public class ArrayQueue<T>
-    {
-        
-    }
+	public class ArrayQueue<T> : IQueue<T>
+	{
+		public int Count => throw new System.NotImplementedException();
+
+		public bool Contains (T element)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public T Dequeue ()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Enqueue (T element)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public T Peek ()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public T[] ToArray ()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
 }

74338ba666ca1ace49080d1b21a6afedad213dcc
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-14 22:38:37 +0200

    Implement array queue constructors.

--- Source/ArrayQueue.cs
+++ Source/ArrayQueue.cs
@@ -1,36 +1,42 @@
-
+using System;
 
 namespace DataStructures.Source
 {
 	public class ArrayQueue<T> : IQueue<T>
 	{
-        private const int DEFAULT_CHUNK_SIZE = 10;
+		private const int DEFAULT_CHUNK_SIZE = 10;
 
-        private T[] internalArray = new T[DEFAULT_CHUNK_SIZE];
-        private readonly int chunkSize = DEFAULT_CHUNK_SIZE;
-        private int lastElementIndex = -1;
+		private T[] internalArray = new T[DEFAULT_CHUNK_SIZE];
+		private readonly int chunkSize = DEFAULT_CHUNK_SIZE;
+		private int lastElementIndex = -1;
 
 		public int Count
-        {
-            get => lastElementIndex + 1;
-        }
+		{
+			get => lastElementIndex + 1;
+		}
 
-        public ArrayQueue() { }
+		public ArrayQueue () { }
 
-        public ArrayQueue(int chunkSize)
-        {
+		public ArrayQueue (int chunkSize)
+		{
+			this.chunkSize = chunkSize;
+			internalArray = new T[chunkSize];
+		}
 
-        }
+		public ArrayQueue (T[] initialElements)
+		{
+			internalArray = new T[initialElements.Length + (initialElements.Length % chunkSize)];
+			Array.Copy(initialElements, internalArray, initialElements.Length);
+			lastElementIndex = initialElements.Length - 1;
+		}
 
-        public ArrayQueue(T[] initialElements)
-        {
-
-        }
-
-        public ArrayQueue(T[] initialElements, int chunkSize)
-        {
-
-        }
+		public ArrayQueue (T[] initialElements, int chunkSize)
+		{
+			this.chunkSize = chunkSize;
+			internalArray = new T[initialElements.Length + (initialElements.Length % chunkSize)];
+			Array.Copy(initialElements, internalArray, initialElements.Length);
+			lastElementIndex = initialElements.Length - 1;
+		}
 
 		public bool Contains (T element)
 		{

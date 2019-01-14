a52223c6c67432640b67e79bbb3943bcac1f3051
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-14 18:09:39 +0200

    Add array queue test stubs.

--- Tests/ArrayQueueTests.cs
+++ Tests/ArrayQueueTests.cs
@@ -3,9 +3,73 @@ using NUnit.Framework;
 
 namespace DataStructures.Tests
 {
-    [TestFixture, TestOf(typeof(ArrayQueue<int>))]
-    public class ArrayQueueTests
-    {
-        
-    }
+	[TestFixture, TestOf(typeof(ArrayQueue<int>))]
+	public class ArrayQueueTests
+	{
+		[Test, Parallelizable]
+		public void CountTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void ContainsTrueTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void ContainsFalseTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void EnqueueTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void EnqueueEmptyTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void DequeueTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void DequeueExceptionTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void PeekTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void PeekExceptionTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void ToArrayTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void ToArrayEmptyTest ()
+		{
+
+		}
+	}
 }

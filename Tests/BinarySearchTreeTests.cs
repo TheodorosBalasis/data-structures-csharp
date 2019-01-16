ad2497119a4a00b7db5f4432203d843a8189be97
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-16 07:31:30 +0200

    Add binary search tree test stubs.

--- Tests/BinarySearchTreeTests.cs
+++ Tests/BinarySearchTreeTests.cs
@@ -3,9 +3,37 @@ using NUnit.Framework;
 
 namespace DataStructures.Tests
 {
-    [TestFixture, TestOf(typeof(BinarySearchTree<int>))]
-    public class BinarySearchTreeTests
-    {
-        
-    }
+	[TestFixture, TestOf(typeof(BinarySearchTree<int>))]
+	public class BinarySearchTreeTests
+	{
+		[Test, Parallelizable]
+		public void InsertTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void ToArrayInOrderTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void ToArrayPreOrderTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void ToArrayPostOrderTest ()
+		{
+
+		}
+
+		[Test, Parallelizable]
+		public void ToArrayEmptyTest ()
+		{
+
+		}
+	}
 }

3b6f7d15b638d9663db8da8577b1060b9b97c357
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-18 21:44:12 +0200

    Implement binary tree node.

--- Source/BinaryTreeNode.cs
+++ Source/BinaryTreeNode.cs
@@ -1,9 +1,15 @@
-
+using System;
 
 namespace DataStructures.Source
 {
-    public class BinaryTreeNode<T>
-    {
-        
-    }
+	public class BinaryTreeNode<T> where T : IComparable
+	{
+		public BinaryTreeNode<T> Left { get; set; } = null;
+		public BinaryTreeNode<T> Right { get; set; } = null;
+		public T Data { get; set; } = default(T);
+
+		public BinaryTreeNode () { }
+
+		public BinaryTreeNode (T data) => Data = data;
+	}
 }

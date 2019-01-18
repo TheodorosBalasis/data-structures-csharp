9bb0cf781c65300dce9608f77a2f5c7d0af61c0e
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-18 22:14:25 +0200

    Implement binary search tree insertion.

--- Source/BinarySearchTree.cs
+++ Source/BinarySearchTree.cs
@@ -4,6 +4,8 @@ namespace DataStructures.Source
 {
 	public class BinarySearchTree<T> where T : IComparable
 	{
+		private BinaryTreeNode<T> root = null;
+
 		public BinarySearchTree () { }
 
 		public BinarySearchTree (T[] initialElements)
@@ -11,10 +13,10 @@ namespace DataStructures.Source
 
 		}
 
-		public void Insert (T element)
-		{
-
-		}
+		/// <exception cref="System.InvalidOperationException">
+		/// Thrown if the element already exists in the tree.
+		/// </exception>
+		public void Insert (T element) => insertRecursive(element, root);
 
 		public T[] ToArrayInOrder ()
 		{
@@ -29,6 +31,29 @@ namespace DataStructures.Source
 		public T[] ToArrayPostOrder ()
 		{
 			throw new NotImplementedException();
+		}
+
+		private void insertRecursive (T element, BinaryTreeNode<T> node)
+		{
+			if (node == null)
+				return;
+
+			if (node.Data.CompareTo(element) == 0)
+				throw new InvalidOperationException("The element already exists!");
+			else if (node.Data.CompareTo(element) > 0)
+			{
+				if (node.Left == null)
+					node.Left = new BinaryTreeNode<T>(element);
+				else
+					insertRecursive(element, node.Left);
+			}
+			else
+			{
+				if (node.Right == null)
+					node.Right = new BinaryTreeNode<T>(element);
+				else
+					insertRecursive(element, node.Right);
+			}
 		}
 	}
 }

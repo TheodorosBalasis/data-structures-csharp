8a8d0fb563deddd2f872d852d9fd951e92dc95ee
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-26 23:30:03 +0200

    Create circular buffer test stubs.

--- Tests/CircularBufferTests.cs
+++ Tests/CircularBufferTests.cs
@@ -6,6 +6,34 @@ namespace DataStructures.Tests
     [TestFixture, TestOf(typeof(CircularBuffer<int>))]
     public class CircularBufferTests
     {
-        
+        [Test, Parallelizable]
+        public void WriteTest ()
+        {
+
+        }
+
+        [Test, Parallelizable]
+        public void WriteEmptyTest ()
+        {
+
+        }
+
+        [Test, Parallelizable]
+        public void ReadTest ()
+        {
+
+        }
+
+        [Test, Parallelizable]
+        public void ToArrayTest ()
+        {
+
+        }
+
+        [Test, Parallelizable]
+        public void ToArrayEmptyTest ()
+        {
+
+        }
     }
 }

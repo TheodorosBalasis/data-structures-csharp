eaab09435a39655104548b33e53d66c87a1010c9
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-26 23:56:16 +0200

    Implement circular buffer tests.

--- Tests/CircularBufferTests.cs
+++ Tests/CircularBufferTests.cs
@@ -9,31 +9,51 @@ namespace DataStructures.Tests
         [Test, Parallelizable]
         public void WriteTest ()
         {
+            var buffer = new CircularBuffer<int>(new int[] { 1, 2, 3, 4 });
 
+            buffer.Write(5);
+
+            Assert.That(buffer.ToArray(), Is.EqualTo(new int[] { 5, 2, 3, 4 }));
         }
 
         [Test, Parallelizable]
         public void WriteEmptyTest ()
         {
+            var buffer = new CircularBuffer<int>(5);
 
+            buffer.Write(1);
+            buffer.Write(2);
+            buffer.Write(3);
+
+            Assert.That(buffer.ToArray(), Is.EqualTo(new int[] { 1, 2, 3 }));
         }
 
         [Test, Parallelizable]
         public void ReadTest ()
         {
+            var buffer = new CircularBuffer<int>(new int[] { 1, 2, 3, 4 });
 
+            Assert.That(buffer.Read(), Is.EqualTo(1));
+            Assert.That(buffer.Read(), Is.EqualTo(2));
+            Assert.That(buffer.Read(), Is.EqualTo(3));
+            Assert.That(buffer.Read(), Is.EqualTo(4));
+            Assert.That(buffer.Read(), Is.EqualTo(1));
         }
 
         [Test, Parallelizable]
         public void ToArrayTest ()
         {
+            var buffer = new CircularBuffer<int>(new int[] { 1, 2, 3, 4 });
 
+            Assert.That(buffer.ToArray(), Is.EqualTo(new int[] { 1, 2, 3, 4 }));
         }
 
         [Test, Parallelizable]
         public void ToArrayEmptyTest ()
         {
+            var buffer = new CircularBuffer<int>(5);
 
+            Assert.That(buffer.ToArray(), Is.EqualTo(new int[] { }));
         }
     }
 }

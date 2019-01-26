dfe6a1fea6f16f45d89dc58a87c0838129c2bfb6
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-01-26 23:26:26 +0200

    Add circular buffer method stubs.

--- Source/CircularBuffer.cs
+++ Source/CircularBuffer.cs
@@ -1,7 +1,32 @@
+using System;
+
 namespace DataStructures.Source
 {
     public class CircularBuffer<T>
     {
-        
+        public CircularBuffer (int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CircularBuffer (T[] initialElements)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write (T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read ()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ToArray ()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

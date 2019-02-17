1bf5d0d87e7e125598abc53af77c6deb164e423a
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-02-17 22:17:05 +0200

    Add linked queue stub.

---
+++ Source/LinkedQueue.cs
@@ -0,0 +1,32 @@
+namespace DataStructures.Source
+{
+    public class LinkedQueue<T> : IQueue<T>
+    {
+        public int Count => throw new System.NotImplementedException();
+
+        public bool Contains (T element)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Dequeue ()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Enqueue (T element)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Peek ()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T[] ToArray ()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

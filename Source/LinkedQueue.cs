c37a70c2e73bcca132a9289f34fd11d1d701b28a
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-02-18 23:41:19 +0200

    Implement LinkedQueue.

--- Source/LinkedQueue.cs
+++ Source/LinkedQueue.cs
@@ -1,32 +1,51 @@
+using System;
+
 namespace DataStructures.Source
 {
     public class LinkedQueue<T> : IQueue<T>
     {
-        public int Count => throw new System.NotImplementedException();
+        private LinkedList<T> internalList = new LinkedList<T>();
+
+        public int Count => internalList.Count;
+
+        public LinkedQueue () { }
+
+        public LinkedQueue (T[] initialElements) => internalList = new LinkedList<T>(initialElements);
 
         public bool Contains (T element)
         {
-            throw new System.NotImplementedException();
+            bool contains = false;
+            for (int i = 0; i < internalList.Count; i++)
+                if (internalList[i].Equals(element))
+                {
+                    contains = true;
+                    break;
+                }
+            return contains;
         }
 
+        public void Enqueue (T element) => internalList.Add(element);
+
+        /// <exception cref="System.InvalidOperationException"> Thrown if queue is empty. </exception>
         public T Dequeue ()
         {
-            throw new System.NotImplementedException();
+            if (Count == 0)
+                throw new InvalidOperationException();
+
+            T element = internalList[0];
+            internalList.Remove(0);
+            return element;
         }
 
-        public void Enqueue (T element)
-        {
-            throw new System.NotImplementedException();
-        }
-
+        /// <exception cref="System.InvalidOperationException"> Thrown if queue is empty. </exception>
         public T Peek ()
         {
-            throw new System.NotImplementedException();
+            if (Count == 0)
+                throw new InvalidOperationException();
+
+            return internalList[0];
         }
 
-        public T[] ToArray ()
-        {
-            throw new System.NotImplementedException();
-        }
+        public T[] ToArray () => internalList.ToArray();
     }
 }
